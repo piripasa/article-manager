7686944cd840a7879f915f6b9082b5e1ee877f5e
Zaman-A-Piri Pasa <pollobcse05@gmail.com>
2018-01-26 14:48:59 +0600

    phpunit tests and travis ci config

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
